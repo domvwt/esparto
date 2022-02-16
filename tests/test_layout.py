8bd94f407c23c9436b4847a8606640af66e22171
Dom <dominic.thorn@gmail.com>
2022-02-16 23:01:00 +0000

    Feature/set column wth dict (#82)
    * Feature/migrate styles to css (#61)
    
    * Drop bootstrap styles, implement in esparto.css
    
    * Ignore test documents
    
    * Bump version in toml
    
    * Refactor redundant regex call
    
    * Update README.md
    
    * Set column as dict
    
    * Fix broken CDN link
    
    * Update testing for HTML output

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -1,3 +1,4 @@
+from copy import copy
 from itertools import chain
 
 import pytest
@@ -193,6 +194,36 @@ def test_set_item_existing_attr(page_basic_layout):
     page["Section One"]["Row One"] = "different content"
     page.section_one.row_one = "markdown content"
     assert page == page_basic_layout
+
+
+def test_set_column_as_dict(page_basic_layout):
+    page = la.Page(title="Test Page")
+
+    expected = page_basic_layout
+    expected[0][0][0] = la.Column(title="Column One", children=["markdown content"])
+
+    page["Section One"]["Row One"]["Wrong Title"] = {"Column One": "markdown content"}
+
+    assert page == expected
+
+
+def test_set_column_as_dict_tuple():
+    page = la.Page(title="Test Page")
+    with pytest.raises(TypeError):
+        page["Section One"]["Row One"]["Column One"] = {
+            "Column One": "markdown content"
+        }, {"Column Two": "markdown content"}
+
+
+def test_set_row_as_dict_tuple():
+    page = la.Page(title="Test Page")
+    expected = copy(page)
+    expected["Section One"]["Row One"]["Column One"] = "markdown content"
+    expected["Section One"]["Row One"]["Column Two"] = "markdown content"
+    page["Section One"]["Row One"] = {"Column One": "markdown content"}, {
+        "Column Two": "markdown content"
+    }
+    assert page == expected
 
 
 def test_delitem_str(page_basic_layout):
