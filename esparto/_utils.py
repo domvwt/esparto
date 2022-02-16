3e090cf3671d42bc5699c1a34ba33147d8f7cd3f
Dom <dominic.thorn@gmail.com>
2022-02-16 19:50:56 +0000

    Feature/set column wth dict (#81)
    * Feature/migrate styles to css (#61)
    
    * Drop bootstrap styles, implement in esparto.css
    
    * Ignore test documents
    
    * Bump version in toml
    
    * Refactor redundant regex call
    
    * Update README.md
    
    * Set column as dict

--- esparto/_utils.py
+++ esparto/_utils.py
@@ -1,15 +1,23 @@
 import re
-from typing import Callable, Dict, Iterable, List
+from typing import TYPE_CHECKING, Any, Callable, Dict, Iterable, List, Optional, Union
+
+if TYPE_CHECKING:
+    from esparto._content import Content
+    from esparto._layout import Layout
 
 
-def get_index_where(condition: Callable[..., bool], iterable: Iterable) -> List[int]:
+def get_index_where(
+    condition: Callable[..., bool], iterable: Iterable[Any]
+) -> List[int]:
     """Return index values where `condition` is `True`."""
     return [idx for idx, item in enumerate(iterable) if condition(item)]
 
 
-def get_matching_titles(title: str, children: list) -> List[int]:
+def get_matching_titles(
+    title: str, children: List[Union["Layout", "Content"]]
+) -> List[int]:
     """Return child items with matching title."""
-    return get_index_where(lambda x: getattr(x, "title", None) == title, children)
+    return get_index_where(lambda x: bool(getattr(x, "title", None) == title), children)
 
 
 def clean_attr_name(attr_name: str) -> str:
@@ -29,18 +37,18 @@ def clean_attr_name(attr_name: str) -> str:
     return attr_name
 
 
-def clean_iterator(iterator: Iterable) -> Iterable:
+def clean_iterator(iterator: Iterable[Any]) -> Iterable[Any]:
     # Convert any non-list iterators to lists
     iterator = (
         list(iterator) if isinstance(iterator, (list, tuple, set)) else [iterator]
     )
-    # Unnest any nested lists of children
+    # Un-nest any nested lists of children
     if len(list(iterator)) == 1 and isinstance(list(iterator)[0], (list, tuple, set)):
         iterator = list(iterator)[0]
     return iterator
 
 
-def public_dict(d: dict) -> dict:
+def public_dict(d: Dict[str, Any]) -> Dict[str, Any]:
     """Remove keys starting with '_' from dict."""
     return {
         k: v for k, v in d.items() if not (isinstance(k, str) and k.startswith("_"))
@@ -52,8 +60,8 @@ def render_html(
     classes: List[str],
     styles: Dict[str, str],
     children: str,
-    identifier: str = None,
-):
+    identifier: Optional[str] = None,
+) -> str:
     """Render HTML from provided attributes."""
     class_str = " ".join(classes) if classes else ""
     class_str = f"class='{class_str}'" if classes else ""
@@ -69,7 +77,9 @@ def render_html(
     return rendered
 
 
-def responsive_svg_mpl(source: str, width: int = None, height: int = None) -> str:
+def responsive_svg_mpl(
+    source: str, width: Optional[int] = None, height: Optional[int] = None
+) -> str:
     """Make SVG element responsive."""
 
     regex_w = r"width=\S*"
@@ -82,8 +92,8 @@ def responsive_svg_mpl(source: str, width: int = None, height: int = None) -> st
     source = re.sub(regex_h, height_, source, count=1)
 
     # Preserve aspect ratio of SVG
-    regexp = r"<svg"
-    repl = '<svg class="svg-content-mpl" preserveAspectRatio="xMinYMin meet" '
-    source = re.sub(regexp, repl, source, count=1)
+    old_str = r"<svg"
+    new_str = '<svg class="svg-content-mpl" preserveAspectRatio="xMinYMin meet" '
+    source = source.replace(old_str, new_str, 1)
 
     return source
---
+++ tests/example_pages.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+import esparto as es
+
+lorem = (
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore "
+    "magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo "
+    "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
+    "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+)
+
+image_path = Path("docs/examples/my-image.png")
+image = es.Image(str(image_path), caption="Photo by Benjamin Voros on Unsplash")
+
+df = pd.DataFrame()
+df["A"] = np.random.normal(10, 4, 1000)
+df["B"] = np.random.normal(-10, 6, 1000)
+df["C"] = np.random.normal(0, 3, 1000)
+df["D"] = np.random.normal(-5, 8, 1000)
+
+df = df
+
+
+def example_01():
+    page = es.Page(title="Test Page", table_of_contents=2)
+
+    page["Section One"]["Row One"]["Column One"] = lorem
+    page["Section Two"] = es.Section()
+    page["Section Three"] = es.CardSection()
+    page["Section Four"] = es.Section()
+    page["Section Five"] = es.CardSection()
+
+    for i in range(1, 5):
+        page["Section Two"][f"Row {i}"] = [(image, lorem) for _ in range(1, i + 1)]
+        page["Section Three"][f"Row {i}"] += [
+            lorem if j % 2 == 0 else {"Card Title": image} for j in range(1, i + 1)
+        ]
+        page["Section Four"][f"Row {i}"] += [
+            {"Column Title": lorem} for _ in range(1, i + 1)
+        ]
+        page["Section Five"][f"Row {i}"] += [
+            {"Card Title": lorem} for _ in range(1, i + 1)
+        ]
+
+    page.save_html("page01.html")
+    page.save_pdf("page01.pdf")
+
+
+def example_02():
+    page = es.Page(title="Test Page")
+
+    _ = df.plot.hist(alpha=0.4, bins=30)
+    fig = plt.gcf()
+    fig.tight_layout()
+
+    page[0][0] = df[:10], fig
+
+    page[0][1] = ({"fig": fig}, {"table": df[:10]})
+
+    page[0][2] = es.CardRowEqual(children=[{"fig": fig}, {"table": df[:10]}][::-1])
+
+    page[0][3] = {"text": lorem}, {"table": df[:10]}
+
+    page.save_html("page02.html")
+    page.save_pdf("page02.pdf")
+
+
+if __name__ == "__main__":
+    example_01()
+    example_02()
