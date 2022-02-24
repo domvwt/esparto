db3846ac505a3ce5f5e80ba4ff981d7e44a02714
Dominic Thorn <dominic.thorn@gmail.com>
2022-02-24 12:56:42 +0000

    Bump version

--- esparto/__init__.py
+++ esparto/__init__.py
@@ -15,7 +15,7 @@ from typing import Set as _Set
 
 __author__ = """Dominic Thorn"""
 __email__ = "dominic.thorn@gmail.com"
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 
 _MODULE_PATH: _Path = _Path(__file__).parent.absolute()
 
